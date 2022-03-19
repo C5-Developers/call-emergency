dc3865f6db1ba39e17a54676ee14814cd151878b
Chrys Redin <redin.dev.87@gmail.com>
2022-03-18 20:34:47 -0600

    Users connected storing in an array

--- app.js
+++ app.js
@@ -13,9 +13,23 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/public/index.html');
 });
 
+let usuariosConectados = [];
+
 io.on('connection', (socket)=> {
-    console.log('user connected to socket.IO server')
-    console.log(socket.id);
+    usuariosConectados.push(socket.id);
+    console.log(usuariosConectados);
+
+    socket.on('disconnect', ()=> { 
+        console.log("Usuario Desconectado"+ socket.id);
+
+        const newUsuarioConectado = usuariosConectados.filter((usuarioSocketId)=>{
+           return usuarioSocketId !== socket.io
+        });
+
+        usuariosConectados = newUsuarioConectado;
+        console.log(usuariosConectados);
+
+    });
 });
 
 server.listen(PORT, ()=>{
